2cdcf1961fb45ddc396cea8ace6f5afcc3482aa5
Trenton D. Adams <trenton.d.adams@gmail.com>
2017-07-19 15:51:01 -0600

    - semi working but not

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 # vi: set noexpandtab:
-all: myfirst.flp
+all: hello.bin
 
 myfirst.bin:  myfirst.asm
 	nasm -f bin -o myfirst.bin myfirst.asm
 
 myfirst.flp:	myfirst.bin
+	dd if=/dev/urandom of=myfirst.flp bs=512 count=10000
 	dd status=noxfer conv=notrunc if=myfirst.bin of=myfirst.flp
 
-test:   myfirst.flp
-	qemu-system-x86_64 -fda myfirst.flp
+hello.bin:
+	gcc hello.c -o hello
+	objcopy -O binary ./hello hello.bin
+#	dd status=noxfer conv=notrunc if=hello.bin of=myfirst.flp seek=1 bs=512
+
+test:   myfirst.flp hello.bin
+	qemu-system-x86_64 -hda myfirst.flp
 
 clean:
-	rm -rf myfirst.flp myfirst.bin
+	rm -rf myfirst.flp myfirst.bin hello hello.bin
 
--- myfirst.asm
+++ myfirst.asm
@@ -12,6 +12,43 @@ start:
 
 	mov si, text_string	; Put string position into SI
 	call print_string	; Call our string-printing routine
+	call read_first_byte  ; read first byte of disk.
+	call read_keys
+	jmp $
+
+
+read_first_byte:
+  push ax
+  push bx 
+  push cx
+  push dx
+  mov ah, 0         ; reset disk
+  mov dl, 80h       ; drive 0
+  int 13h
+  jnz disk_fail     ; return, we're a failure
+  mov ax, 0201h     ; int 13h 02 = read disk  and 01 = sectors to read
+  mov cx, 0002h     ; first track second sector - one past boot
+  mov dx, 0080h     ; 00 = head number + 80 = first drive
+  mov bx, 512      ; place in memory 512 bytes past where the MBR is loaded.
+  int 13h
+
+;;  mov ds, es
+  mov si, 512       ; prep lodsb
+  lodsb             ; load the first byte of the disk sector read - sector 2
+	and ax, 000fh      ; just clear the top of the byte
+	mov bx, ax        ; move byte read into base register
+	mov ax, 2         ; multiply by 2
+	mul bx
+	add bx, hex_ascii ;
+  mov si, bx
+  call print_string
+
+disk_fail:
+  pop dx
+  pop cx
+  pop bx 
+  pop ax
+  ret
 
 read_keys:
   mov ah, 01h       ; detect key
@@ -20,13 +57,14 @@ read_keys:
 	jmp read_keys			; Jump to read_keys - infinite loop!
 
 print_key:
-  mov ah, 0         ; read key
-  int 16h           ; ah now has character from keyboard
-  mov ah, 0Eh       ; TTY output
+  mov ah, 0         ; 16h read key function
+  int 16h           ; al now has character from keyboard
+  mov ah, 0Eh       ; TTY output, ah had scan code, we discard
   int 10h           ; prints character in ah
   jmp read_keys
 
 print_string:			; Routine: output string in SI to screen
+  push ax
 	mov ah, 0Eh		; int 10h 'print char' function
 
 .repeat:
@@ -37,10 +75,26 @@ print_string:			; Routine: output string in SI to screen
 	jmp .repeat
 
 .done:
+  pop ax
 	ret
 
 	text_string db 'This is my cool new OS!', 0
-	dot db '.', 0  ; 2E00
+	hex_ascii db '0',0
+	db '1',0
+	db '2',0
+	db '3',0
+	db '4',0
+	db '5',0
+	db '6',0
+	db '7',0
+	db '8',0
+	db '9',0
+	db 'A',0
+	db 'B',0
+	db 'C',0
+	db 'D',0
+	db 'E',0
+	db 'F',0
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		; The standard PC boot signature
