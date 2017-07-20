5fb508f26025285dda26c7dafee4eeebc813bd38
Trenton D. Adams <trenton.d.adams@gmail.com>
2017-07-19 21:18:50 -0600

    - minor swapping around of things to be more clear

--- myfirst.asm
+++ myfirst.asm
@@ -1,8 +1,10 @@
 BITS 16
 
 %macro print 1
+  pusha
 	mov si, %1	        ; Put string position into SI
 	call print_string	    ; Call our string-printing routine
+	popa
 %endmacro
 
 start:
@@ -15,8 +17,9 @@ start:
 	mov ds, ax
 
 	mov [boot_drive], dl  ; store the boot drive in the one byte buffer
+	call read_disk_stats
 
-  print text_string
+  print boot_msg
   print crlf
   
 	call read_first_byte  ; read first byte of disk.
@@ -25,11 +28,18 @@ start:
 
 read_disk_stats:
   pusha
+  ;;print stats
+;;  xor ax, ax
+;;  xor dx, dx
   mov ah, 8
+  mov dl, [boot_drive]
   int 13h
   jz ds_rf
+  print stats_complete
+  jmp ds_done
 ds_rf:
-  call read_fail
+  call int13_show_error
+ds_done:
   popa
   ret
 
@@ -41,7 +51,7 @@ read_first_byte:
   jz reset_fail      ; return, we're a failure
   jmp reset_success
 reset_fail:
-  call read_fail
+  call int13_show_error
   jmp disk_return
 reset_success:
   mov ax, 0x0201     ; int 13h 02 = read disk  and 01 = sectors to read
@@ -51,7 +61,7 @@ reset_success:
   int 0x13
   jz rs_fail
 rs_fail:
-  call read_fail
+  call int13_show_error
   jmp disk_return
 
 ;;  mov ds, es
@@ -59,15 +69,14 @@ rs_fail:
 ;;  lodsb             ; load the first byte of the disk sector read - sector 2
   jmp disk_return
 
-read_fail:
-  ; make subroutine to handle all int 13h failures
-  ;
+int13_show_error:
   pusha
   push ax
   print int13_call_fail
   print int13_read_status
   pop ax
 
+;; make hex display callable/reusable
   mov bx, hex_ascii           ; lookup table
   mov dl, ah                  ; store int13 status for later (high byte of ax)
 	mov ah, al                  ; copy high nibble to ah
@@ -77,13 +86,13 @@ read_fail:
 	xchg al, ah                 ; swap the high/low nibble
 	xlat                        ; lookup high nibble in table pointed to by bx.
 	xchg al, ah                 ; swap ascii representations back to proper pos
-	lea bx, [gsb]               ; general status buffer
+	lea bx, [buffer]               ; general status buffer
 	mov [bx+2], ax                ; store ax (ascii hex of AX) in the buffer
 	mov ax, 0x0000              ; null bytes
 	mov [bx + 4],ax             ;     for end of string
 	mov ax, '0x'                ; prefix entire
 	mov [bx],ax                 ;     string with 0x
-	print gsb                   ; print gsb string
+	print buffer                   ; print gsb string
   print crlf
   popa
   ret
@@ -123,18 +132,19 @@ print_string:			; Routine: output string in SI to screen
   pop ax
 	ret
 
-	text_string db 'Bootstrapping is sexy...', 0
+	boot_msg db 'Bootstrapping is sexy...', 0
 	int13_call_fail db 'Disk failure!',0x0a,0x0d, 0
 	int13_read_status db 'Call status: ', 0
+	stats_complete db 'Boot drive found', 0x0a, 0x0d,0
+	stats db 'read stats', 0x0a, 0x0d,0
 	crlf db 0x0a,0x0d,0
 
 	boot_drive db 0x00
-
-	; general status buffer
-	gsb times 64 db 0
 
 	; hex to ascii table
 	hex_ascii db '0123456789ABCDEF',0
 
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		; The standard PC boot signature
+
+buffer:
